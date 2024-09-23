5eb0be03953f24fcaa0a153c45aaaefb34fd799c
Peterlee10 <peterogundipe84@gmail.com>
2024-09-23 14:08:57 +0100

    Burger

---
+++ burger.js
@@ -0,0 +1,17 @@
+
+// Created by Peterlee10
+
+
+
+
+addEventListener("DOMContentLoaded",()=>{
+    
+});
+
+function openNav(){
+  document.querySelector(".sidenav").style.width="75%"
+}
+function closeNav(){
+  document.querySelector(".sidenav").style.width="0%"
+
+}
